4ea6c5d677fe2abc2ab7c693025f34375c1e663d
Anthony Bartholomaus <a.l.bartholomaus@outlook.com>
2024-06-14 15:28:43 -0500

    Add project files.

---
+++ LearningBasics/1.BasicsOrBasics/Tree/CreateTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningBasics.BasicsOrBasics.Tree
+{
+    public class CreateTree
+    {
+        public TreeNode tree;
+        public TreeNode Insert(TreeNode root, int value)
+        {
+            return tree;
+        }
+        public TreeNode Remove(TreeNode root, int value)
+        {
+            return root;
+        }
+        public class TreeNode
+        {
+            public TreeNode()
+            {
+
+            }
+        }
+    }
+}
+
---
+++ LearningBasics/3.TopAlgorithm/SlidingWindowVariableSize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningBasics._3.TopAlgorithm
+{
+    public class SlidingWindowVariableSize
+    {
+
+    }
+}
